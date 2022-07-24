9ccc7512a32aef8f5bb8f38bd0d9a087d5facef1
Mohit Bagdi <92544103+edito0@users.noreply.github.com>
2022-07-25 00:22:42 +0530

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import '../header.css'
+import logo from '../aavesh_logo.jpeg';
 
-function Header() {
-  return (
-    <>
-      
-    </>
-  ); 
+function Header() { 
+
+    function menu() {
+        const humbuerger = document.querySelector('.humburger');
+        const humbuergerLine = document.querySelectorAll('.humburger-line');
+
+        humbuergerLine.forEach((element) => {
+            element.classList.toggle('menu');
+        });
+
+        const navbar = document.querySelector('.nav__list__container')
+        navbar.classList.toggle('nav__list__container-menu');
+        humbuerger.classList.toggle('humburger-menu');;
+    }
+
+
+
+    return (
+        <>
+            <header class="home__header">
+                <nav>
+                    <div class="nav__logo__container">
+                        <img  src={logo} alt="" />
+                    </div> 
+
+                    <div class="nav__list__container">
+                        <ul>
+                            <li><a href="index.html">HOME</a></li>
+                            <li><a href="products.html">PRODUCTS</a></li>
+                            <li><a href="about.html">ABOUT US</a></li>
+                            <li><a href="gallery.html">GALLERY</a></li>
+                            <li><a href="contact-us.html">CONTACT US</a></li>
+                        </ul>
+                    </div>
+
+                </nav>
+
+                <div class='humburger' onclick="menu()">
+                    <div class='humburger-line'></div>
+                    <div class='humburger-line'></div>
+                    <div class='humburger-line'></div>
+                </div>
+
+            </header>
+        </>
+    );
 }
 
 export default Header;
